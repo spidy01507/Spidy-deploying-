3e1a2f4a80aecc12974b98f483e3d47e08d771cd
spidy01507 <spidy01507@gmail.com>
2025-03-30 12:48:07 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-
+# Update package list and install dependencies
 apt-get update 
 
 apt-get install -y cmake g++ make \
     libopenblas-dev liblapack-dev \
     libx11-dev libgtk-3-dev libboost-all-dev
 
+# Upgrade CMake
 pip install --upgrade cmake
+apt-get install -y cmake
 
-pip install dlib==19.24.2
+# Check CMake version
+cmake --version
 
+# Install dlib with explicit CMake policy version
+CMAKE_POLICY_VERSION_MINIMUM=3.5 pip install dlib==19.24.2
+
+# Install other dependencies
 pip install --no-cache-dir -r requirements.txt
