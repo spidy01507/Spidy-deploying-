1516eb00a82e8f21350242a625efeb34ca9940f3
spidy01507 <spidy01507@gmail.com>
2025-03-30 12:02:57 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,14 +1,6 @@
-#!/usr/bin/env bash
+# Install required system dependencies
+apt-get update && apt-get install -y cmake libx11-dev libatlas-base-dev libgtk2.0-dev libboost-all-dev python3-dev
 
-# Update package lists and install system dependencies for dlib
-apt-get update && apt-get install -y \
-    cmake \
-    libx11-dev \
-    libatlas-base-dev \
-    libgtk2.0-dev \
-    libboost-all-dev \
-    python3-dev
-
-# Install Python dependencies
-pip install --upgrade pip
+# Install prebuilt dlib
+pip install --no-cache-dir dlib==19.24.2
 pip install --no-cache-dir -r requirements.txt
