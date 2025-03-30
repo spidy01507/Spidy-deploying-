6613a3282b0b641d5b4249520eeb2054276c0a91
spidy01507 <spidy01507@gmail.com>
2025-03-30 12:38:43 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-# Update package list
+
 apt-get update 
 
-# Install dependencies for dlib
 apt-get install -y cmake g++ make \
     libopenblas-dev liblapack-dev \
     libx11-dev libgtk-3-dev libboost-all-dev
 
-# Force reinstall CMake (if needed)
 pip install --upgrade cmake
 
-# Install dlib with specific version
 pip install dlib==19.24.2
 
-# Install other Python dependencies
 pip install --no-cache-dir -r requirements.txt
